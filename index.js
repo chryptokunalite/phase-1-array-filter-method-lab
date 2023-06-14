d51c385b203a6a4af3dd386c858ab40d819dbb9c
Allan Saenz <allan.d.saenz@gmail.com>
2023-06-14 18:14:05 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution here
+function findMatching(array, string){
+    let filteredArray = array.filter(function(name){
+        return string.toLowerCase() === name.toLowerCase()
+    })
+    return(filteredArray)
+}
+
+function fuzzyMatch(drivers, string){
+    console.log(drivers)
+    console.log(string)
+    let filteredArray = drivers.filter(function(name){
+        return name.toLowerCase().indexOf(string.toLowerCase()) === 0
+    })
+    console.log(filteredArray)
+    return(filteredArray)
+}
+
+function matchName(drivers, string){
+    console.log(drivers)
+    console.log(string)
+    let filteredArray = drivers.filter(function(object){
+        return object.name === string 
+    })
+return(filteredArray)
+}
